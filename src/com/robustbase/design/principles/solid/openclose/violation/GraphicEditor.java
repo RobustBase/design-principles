0b2c5521194786c7de2a9c0c564e3376fe83c93d
Atul Dwivedi <atul16dwivedi@gmail.com>
2021-07-11 11:49:02 +0530

    removing comment

--- src/com/robustbase/design/principles/solid/openclose/violation/GraphicEditor.java
+++ src/com/robustbase/design/principles/solid/openclose/violation/GraphicEditor.java
@@ -24,24 +24,6 @@ public class GraphicEditor {
     public static void main(String[] args) {
         GraphicEditor graphicEditor = new GraphicEditor();
         graphicEditor.drawShape(new Rectangle());
-
-        /*
-         * What if a new shape need to be introduced, let's say Triangle?
-         *
-         * We need to do below steps in order to make it work with existing design
-         * 1. Create Triangle class
-         * 1.1 Extend from Shape
-         * 1.2 Initialize type instance variable of Shape class in Triangle constructor
-         * 1.3 create drawTriangle method
-         *
-         * 2. Modify GraphicEditor class
-         * 2.1 Add one more else-if block for Triangle
-         * 2.2 Add drawTriangle method
-         *
-         * Point number 2. Modify GraphicEditor class represent bad design
-         * What problems this bad design
-         */
-
     }
 }
 
